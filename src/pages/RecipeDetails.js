138d232e7e6110ffd8d809d43e08d5ac29899f69
Joanne Lin <joannelin@MacBook-Pro.hitronhub.home>
2022-06-08 19:29:26 -0700

    added title, image, and instructions to recipedetails page

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -2,24 +2,33 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import { Button, Info } from "react";
 
-const RecipeDetails = (props) => {
+const RecipeDetails = () => {
   let params = useParams();
   const [details, setDetails] = useState({});
+  const [activeTab, setActiveTab] = useState("ingredients");
 
   const fetchDetails = async () => {
     const data = await axios.get(
       `https://api.spoonacular.com/recipes/${params.id}/information?apiKey=${process.env.REACT_APP_API_KEY}`
     );
     setDetails(data);
-    console.log(details);
+    console.log(data);
   };
 
   useEffect(() => {
     fetchDetails();
   }, [params.id]);
 
-  return <div>{details.data.title}</div>;
+  return (
+    <div>
+      {/* <h2>{details.data.title}</h2>
+      <img src={details.data.image}></img>
+      <p dangerouslySetInnerHTML={{ __html: details.data.instructions }}></p>
+      <button>Save to Favourite</button> */}
+    </div>
+  );
 };
 
 export default RecipeDetails;
